241b391aacb922abac10a319519ab6b73b252ed5
Abdul-Razak Adams <abdulrazakadams28@gmail.com>
2023-12-21 16:20:12 +0000

    first commit

---
+++ js/index.js
@@ -0,0 +1,28 @@
+// toggle overlay with class
+// function closeMenu(){ 
+//     document.querySelector('.overlay-nav').classList.remove('active')
+// }
+// function openMenu(){
+//     console.log(
+//         document.querySelector('.overlay-nav').classList.add('active')
+//     );
+// }
+
+// optimised class toggle
+
+function toggleOverlay(){
+    document.querySelector('.overlay-nav').classList.toggle('active')
+}
+
+
+
+// toggle overlay with styling
+
+// function closeMenu(){ 
+//     document.querySelector('.overlay-nav').style.display='none'
+// }
+// function openMenu(){
+    
+//         document.querySelector('.overlay-nav').style.display='block'
+
+// }
